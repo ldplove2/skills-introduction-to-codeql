8069089ae1e14240d7d87360e81eb7771cc52aaf
ldplove2 <Peterson_Lola@bah.com>
2026-08-26 15:59:01 -0400

    Update routes.py

--- server/routes.py
+++ server/routes.py
@@ -15,6 +15,7 @@ def index():
         cursor.execute(
             "SELECT * FROM books WHERE name LIKE '%" + name + "%'"
         )
+        
         books = [Book(*row) for row in cursor]
 
     elif author:
